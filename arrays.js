dd54a836739e69d937e84a159f64377fbc2d4156
Xochitl Fierros <xochitlfm@gmail.com>
2017-10-10 20:47:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,34 +5,34 @@ newArray = [element, ...array];
 return newArray
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
+function destructivelyAddElementToBeginningOfArray(array, element) {
 array.unshift(element);
 return array
 }
 
-function addElementToEndOfArray(array,element){
+function addElementToEndOfArray(array, element) {
   newArray = [...array, element];
   return newArray
 }
 
-function destructivelyAddElementToEndOfArray(array, element){
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array
 }
 
-function accessElementInArray(array, index){
+function accessElementInArray(array, index) {
   return array[index];
 }
-function destructivelyRemoveElementFromBeginningOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array
 }
-function removeElementFromBeginningOfArray(array){
+function removeElementFromBeginningOfArray(array) {
   newArray = array.slice(1);
   return newArray
 }
-function destructivelyRemoveElementFromEndOfArray(array){
-  newArray = array.pop();
+function destructivelyRemoveElementFromEndOfArray(array) {
+ array.pop();
   return newArray
 }
 
